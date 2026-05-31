fd5f626afcf1b8ceb30e84980340af259e9ca8df
juanpsama <samajpablo@gmail.com>
2026-05-31 16:05:31 -0600

    feat: update site and base to serve on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://garpe.dev',
+  site: 'https://juanpsama.github.io',
+  base: '/landing-consulting',
   vite: {
     plugins: [tailwindcss()]
   },
